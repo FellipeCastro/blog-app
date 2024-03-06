25b4e36454d77a41bc9235a9da20c6f6d4fc71c3
FellipeCastro <fellipecastro27d@gmail.com>
2024-03-06 18:09:29 -0300

    aula06 finalizada

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const handlebars = exphbs.create({})
 
@@ -7,6 +8,17 @@ app.engine('handlebars', handlebars.engine)
 app.set('view engine', 'handlebars')
 
 const PORT = process.env.PORT || 5000
+
+// MongoDB
+
+const Schema = mongoose.Schema
+mongoose.conect(process.envMONGO_DB, 
+    {useNewUrlParser: true})
+    .then(() => {
+        console.log('MongoDb Rodando')
+    }).catch((err) => {
+        console.log(`Erro ao conectar com MongoDB: ${err}`)
+    })
 
 app.get('/', (req, res) => {
     res.render('formulario')
