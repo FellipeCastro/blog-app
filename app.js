e8b7716409404e0076bb6fdeeb8fe2364cf8c774
FellipeCastro <fellipecastro27d@gmail.com>
2024-03-08 17:21:57 -0300

    aula 07 finalizada

--- app.js
+++ app.js
@@ -4,15 +4,19 @@ const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const handlebars = exphbs.create({})
 
+const { categorias } = require('./models/Categoria.js')
+
+// ENV com dados do Mongo
+require('dotenv').config()
+
 app.engine('handlebars', handlebars.engine)
 app.set('view engine', 'handlebars')
 
 const PORT = process.env.PORT || 5000
 
-// MongoDB
-
+// Conexão com MongoDB
 const Schema = mongoose.Schema
-mongoose.conect(process.envMONGO_DB, 
+mongoose.conect(process.env.MONGO_DB, 
     {useNewUrlParser: true})
     .then(() => {
         console.log('MongoDb Rodando')
