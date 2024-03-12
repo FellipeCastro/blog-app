73407a8ca3020b0403fa444796e2b9d8b10b4b85
FellipeCastro <fellipecastro27d@gmail.com>
2024-03-12 17:55:17 -0300

    aula 15 finalizada

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 // Models
 const { usuarios } = require('../models/Usuario.js')
@@ -20,13 +21,26 @@ router.get('/usuario/add', (req, res) => {
 })
 
 router.post('/usuario/novo', (req, res) => {
+    let salt = bcrypt.genSaltSync(10)
+    let hash = bcrypt.hashSync(req.body.senha, salt)
+    
     const novoUsuario = new usuarios({
         nome: req.body.nome,
         email: req.body.email,
-        senha : req.body.senha
+        senha : hash
     })
     novoUsuario.save()
     res.redirect('/admin/usuarios')
+})
+
+router.post('/usuario/apagar', (req, res) => {
+    usuarios
+        .deleteOne({
+            _id: req.body.id
+        })
+        .then(() => {
+            res.redirect('/admin/usuarios')
+        })
 })
 
 module.exports = router
